6946e88c68a504f31a115f3a131c1a4a87d5e234
Razvan Cristea <razvancristea@192-168-0-107.rdsnet.ro>
2019-08-24 11:51:17 +0300

    changed SECRET_KEY

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 DEBUG = True #Enables debug mode. To be changed to false when deploying to production
 
-SECRET_KEY = 'W34llL1v1ng1n4Y3ll0wS_bm4r1n3!'
+SECRET_KEY = 'ThisIsASecretKey!'
 
 WTF_CSRF_ENABLED = True
